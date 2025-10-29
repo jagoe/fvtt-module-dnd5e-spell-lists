17475401a23bc19e7d9d6b6affe46099ae590874
Jakob Goebel <goebel.jakob@gmail.com>
2025-10-29 08:24:59 +0100

    Initialize file structure w/ correct names

--- .github/ISSUE_TEMPLATE/bug_report.md
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -3,7 +3,7 @@ name: Bug report
 about: Create a report to help us improve
 title: ''
 labels: bug
-assignees: DFreds
+assignees: jagoe
 
 ---
 
@@ -16,7 +16,7 @@ A clear and concise description of what the bug is.
 - Screenshots and demonstrating the problem if applicable.
 - Any errors in the console; access it by pressing F12
 - Whether you ran [Find the Culprit](https://foundryvtt.com/packages/find-the-culprit/) and if it was caused by an interaction with another module
-- Foundry version (i.e. 11.315)
+- Foundry version (i.e. 13.350)
 - System and version (i.e. D&D 5e 2.4.1)
 - Module version (include all modules involved)
 - Browser and hosting service
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import packageJSON from "./package.json" with { type: "json" };
 
-const PACKAGE_ID = "modules/dfreds-module-template-ts";
+const PACKAGE_ID = "modules/fvtt-module-dnd5e-spell-lists";
 
 const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
     const buildMode =
@@ -50,11 +50,11 @@ const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
         fs.writeFileSync("./index.html", `<h1>${message}</h1>\n`);
         if (!fs.existsSync("./styles")) fs.mkdirSync("./styles");
         fs.writeFileSync(
-            "./styles/dfreds-module-template-ts.css",
+            "./styles/fvtt-module-dnd5e-spell-lists.css",
             `/** ${message} */\n`,
         );
         fs.writeFileSync(
-            "./dfreds-module-template-ts.mjs",
+            "./fvtt-module-dnd5e-spell-lists.mjs",
             `/** ${message} */\n\nwindow.global = window;\nimport "./src/ts/module.ts";\n`,
         );
         fs.writeFileSync("./vendor.mjs", `/** ${message} */\n`);
@@ -62,7 +62,7 @@ const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
 
     return {
         base:
-            command === "build" ? "./" : `/modules/dfreds-module-template-ts/`,
+            command === "build" ? "./" : `/modules/fvtt-module-dnd5e-spell-lists/`,
         publicDir: "static",
         define: {
             BUILD_MODE: JSON.stringify(buildMode),
@@ -74,7 +74,7 @@ const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
             minify: false,
             sourcemap: buildMode === "development",
             lib: {
-                name: "dfreds-module-template-ts",
+                name: "fvtt-module-dnd5e-spell-lists",
                 entry: "src/ts/module.ts",
                 formats: ["es"],
                 fileName: "module",
@@ -86,9 +86,9 @@ const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
                     /^@common\//,
                 ],
                 output: {
-                    assetFileNames: "styles/dfreds-module-template-ts.css",
+                    assetFileNames: "styles/fvtt-module-dnd5e-spell-lists.css",
                     chunkFileNames: "[name].mjs",
-                    entryFileNames: "dfreds-module-template-ts.mjs",
+                    entryFileNames: "fvtt-module-dnd5e-spell-lists.mjs",
                     manualChunks: {
                         vendor: Object.keys(packageJSON.dependencies)
                             ? Object.keys(packageJSON.dependencies)
@@ -114,7 +114,7 @@ const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
             port: 30001,
             open: false,
             proxy: {
-                "^(?!/modules/dfreds-module-template-ts/)":
+                "^(?!/modules/fvtt-module-dnd5e-spell-lists/)":
                     "http://localhost:30000/",
                 "/socket.io": {
                     target: "ws://localhost:30000",
@@ -158,7 +158,7 @@ function deleteLockFilePlugin(): Vite.Plugin {
             const outDir = outputOptions.dir ?? "";
             const lockFile = path.resolve(
                 outDir,
-                "dfreds-module-template-ts.lock",
+                "fvtt-module-dnd5e-spell-lists.lock",
             );
             fs.rmSync(lockFile);
         },
